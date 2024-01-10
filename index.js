f3162c0babaa4355e62812327de550c8c4fd5dea
delvontedixon <delvontedixon@yahoo.com>
2024-01-10 11:53:36 -0500

    classes added

---
+++ index.js
@@ -0,0 +1,14 @@
+class House {
+  constructor(name) {
+    this.name = name;
+    this.rooms = [];
+  }
+}
+
+class Room {
+  constructor(name, area) {
+    this.name = name;
+    this.area = area;
+  }
+}
+//Above are the two classes//
