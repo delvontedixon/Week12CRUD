f580b5cb9627550713c5bc6d20640087f4c0510e
delvontedixon <delvontedixon@yahoo.com>
2024-01-10 12:13:27 -0500

    get create update delete

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ class House {
     this.name = name;
     this.rooms = [];
   }
+
+  addRoom(name, area) {
+    this.rooms.push(new Room(name, area));
+  }
 }
 
 class Room {
@@ -12,3 +16,33 @@ class Room {
   }
 }
 //Above are the two classes//
+class HouseService {
+  static url = "https://ancient-taiga-31359.herokupp.com/api/houses";
+
+  static getAllHouses() {
+    return $.get(this.url);
+  }
+
+  static getHouse(id) {
+    return $.get(this.url + `/${id}`);
+  }
+  static createHouse(house) {
+    return $.post(this.url, house);
+  }
+  static updateHouse(house) {
+    return $.ajax({
+      url: this.url + `/${house._id}`,
+      dataType: "json",
+      data: JSON.stringify(house),
+      contentType: "application/json",
+      type: "PUT",
+    });
+  }
+
+  static deleteHouse(id) {
+    return $.ajax({
+      url: this.url + `/${id}`,
+      type: "DELETE",
+    });
+  }
+}
